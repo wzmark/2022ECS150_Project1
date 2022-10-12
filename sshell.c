2bc4b5e3d1952ec62bb57711c04426a2d88b3090
wzmark <414839641wz@gmail.com>
2022-10-12 13:03:40 -0700

    fix try 4

--- sshell.c
+++ sshell.c
@@ -557,7 +557,7 @@ void ViewStart(){
 
 
         while(fgets(userInput, CMD_MAX_LEN, stdin) != NULL){
-                //printf("%s", userInput);
+                printf("%s", userInput);
                 fflush(stdout);
                 //find postion of \n and set it to 0 for deleting 
                 userInput[strcspn(userInput, "\n")] = 0;
